1934df838e062fe5e2baf8caae6ef5105150f3f6
Hakuamesan <hakuame@gmail.com>
2023-08-16 20:41:49 +0100

    updated library info

--- src/AsyncElegantOTA.h
+++ src/AsyncElegantOTA.h
@@ -1,23 +1,16 @@
 #ifndef AsyncElegantOTA_h
 #define AsyncElegantOTA_h
 
-#warning AsyncElegantOTA.loop(); is deprecated, please remove it from loop() if defined. This function will be removed in a future release.
-
-#include "Arduino.h"
-#include "stdlib_noniso.h"
-
-#ifdef defined(ESP32)
-    #include "WiFi.h"
-    #include "AsyncTCP.h"
-    #include "Update.h"
-    #include "esp_int_wdt.h"
-    #include "esp_task_wdt.h"
-#endif
-
+#include <Arduino.h>
+#include <stdlib_noniso.h>
+#include <WiFi.h>
+#include "AsyncTCP.h"
+#include "Update.h"
+#include "esp_int_wdt.h"
+#include "esp_task_wdt.h"
 #include "Hash.h"
 #include "ESPAsyncWebServer.h"
 #include "FS.h"
-
 #include "elegantWebpage.h"
 
 
